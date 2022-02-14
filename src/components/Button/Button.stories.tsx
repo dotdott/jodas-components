27c49a2deb1c85eba7bf3d8185073532ad54ab6a
Gabriel Jodas <70294805+dotdott@users.noreply.github.com>
2022-02-14 14:22:52 -0300

    feat: testing button overwriting styles ++ no text format decorations

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import Button, { IButtonProps } from "./Button";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
@@ -9,10 +7,23 @@ export default {
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args: IButtonProps) => (
-  <Button {...args}>{args.children}</Button>
+  <Button {...args}>{args.children ?? "Readonly Button!"}</Button>
 );
 
 export const Default = Template.bind({});
-Default.args = {
-  children: "Readonly Button!",
+
+export const RedButtonNoTextFormat = Template.bind({});
+RedButtonNoTextFormat.args = {
+  noUppercase: true,
+  btnClasses: "_red",
+};
+
+export const CustomButton = Template.bind({});
+CustomButton.args = {
+  btnExtraStyles: {
+    padding: "16px 32px",
+    background: "linear-gradient(45deg, yellow 30%, white 90%)",
+    color: "black",
+    borderRadius: "16px",
+  },
 };

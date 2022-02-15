10d0b8d9ae91d75a3b96b85a00b8814690222f25
Gabriel Jodas <70294805+dotdott@users.noreply.github.com>
2022-02-15 10:30:08 -0300

    feat: label in storie book indicating where user can find and test other icons

--- src/components/Icons/Icons.stories.tsx
+++ src/components/Icons/Icons.stories.tsx
@@ -7,7 +7,20 @@ export default {
 } as ComponentMeta<typeof Icons>;
 
 const Template: ComponentStory<typeof Icons> = (args: IIConsProps) => (
-  <Icons {...args} />
+  <div style={{ display: "flex", flexDirection: "column" }}>
+    <p>
+      You can see others icons in{" "}
+      <a
+        href="https://fonts.google.com/icons?selected=Material+Icons"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Google fonts icons
+      </a>{" "}
+      and just have to switch the name of the icon in the controls
+    </p>
+    <Icons {...args} />
+  </div>
 );
 
 export const Default = Template.bind({});

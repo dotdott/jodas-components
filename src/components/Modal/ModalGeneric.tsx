6f66df1b8caa78d13f60f6aabcc0106d76f30fdb
Gabriel Jodas <70294805+dotdott@users.noreply.github.com>
2022-02-15 14:38:08 -0300

    feat: generic modal component created ++ tests specs

---
+++ src/components/Modal/ModalGeneric.tsx
@@ -0,0 +1,85 @@
+import { Modal } from "@material-ui/core";
+import Button from "../Button";
+import Icons from "../Icons";
+import "./styles.scss";
+
+export interface IModalGeneric {
+  show: boolean;
+  handleClose: () => void;
+  handleConfirm?: () => void;
+  modalMessage?: string;
+  modalRef: any;
+  texts?: {
+    title?: string;
+    btnLeft?: string;
+    btnRight?: string;
+  };
+  hideLeftBtn?: boolean;
+  hideRightBtn?: boolean;
+  leftBtnFun?: () => void;
+}
+
+const ModalGeneric = ({
+  show,
+  handleClose,
+  modalMessage,
+  modalRef,
+  handleConfirm,
+  texts = {
+    title: "Aviso:",
+    btnLeft: "CANCELAR",
+    btnRight: "CONFIRMAR",
+  },
+  hideLeftBtn = false,
+  hideRightBtn = false,
+  leftBtnFun,
+}: IModalGeneric) => {
+  return (
+    <Modal open={show} onClose={handleClose}>
+      <div className="modal__wrapper" ref={modalRef}>
+        <div className="modal__wrapper__background">
+          <div className="modal__wrapper__header">
+            <div className="modal__wrapper__header__message">
+              <span>{texts.title}</span>
+
+              <Icons name="close" handleClick={handleClose} />
+            </div>
+            <div className="modal__wrapper__body">
+              <p className="modal__wrapper__body__message">{modalMessage}</p>
+            </div>
+
+            <div className="modal__wrapper__footer">
+              {!hideLeftBtn && (
+                <Button
+                  btnClasses="_red"
+                  btnExtraStyles={{
+                    height: 35,
+                    width: hideRightBtn ? "98%" : "48%",
+                  }}
+                  btnFunction={leftBtnFun ?? handleClose}
+                >
+                  {texts.btnLeft}
+                </Button>
+              )}
+
+              {!hideRightBtn && (
+                <Button
+                  btnClasses="_blue"
+                  btnExtraStyles={{
+                    height: 35,
+                    width: hideLeftBtn ? "98%" : "48%",
+                  }}
+                  btnFunction={handleConfirm}
+                >
+                  {texts.btnRight}
+                </Button>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default ModalGeneric;
